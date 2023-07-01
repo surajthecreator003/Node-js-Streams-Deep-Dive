f011c5dddae5d0417d9e7f6a463f63bbddef993c
Suraj Mallick <surajhustlermallick@gmail.com>
2023-07-01 17:14:00 +0530

    second push with test.txt

--- writemany.js
+++ writemany.js
@@ -1,9 +1,17 @@
-console.time("hey");
-
-for(let i=1;i<50;i++){
-console.log(i);
+const fs = require("node:fs/promises");
 
 
-}
+//takes 20 sec for i < 1000000 and takes 45 MB RAM max
+(async ()=>{
+    console.time("writeMany");
 
-console.timeEnd("hey");
+    const fileHandle=await fs.open("test.txt","w");
+
+    for (let i=1;i<1000000;i++){
+       await fileHandle.write(` ${i} `);
+    }
+
+
+
+    console.timeEnd("writeMany");
+})()
